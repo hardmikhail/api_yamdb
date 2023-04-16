d9a6cd264bf4073cc537bd715183037d07149524
Mikhail <mikhail@sudarkin.com>
2023-04-16 22:39:31 +0500

    Refactoring.

--- api_yamdb/api/authentication.py
+++ api_yamdb/api/authentication.py
@@ -1,52 +1,8 @@
-from datetime import datetime, timedelta
-
-import jwt
-from django.conf import settings
-from rest_framework import authentication
-from rest_framework.exceptions import AuthenticationFailed, ParseError
-
-from .models import User
+from rest_framework_simplejwt.tokens import AccessToken
 
 
-class JWTAuthentication(authentication.BaseAuthentication):
-    def authenticate(self, request):
-        jwt_token = request.META.get('HTTP_AUTHORIZATION')
-        if jwt_token is None:
-            return None
-
-        jwt_token = JWTAuthentication.get_the_token_from_header(jwt_token)
-
-        try:
-            payload = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=['HS256'])
-        except jwt.exceptions.InvalidSignatureError:
-            raise AuthenticationFailed('Invalid signature')
-        except:
-            raise ParseError()
-
-        username = payload.get('username')
-        user = User.objects.filter(username=username).first()
-        if user is None:
-            raise AuthenticationFailed('User not found')
-
-        return user, payload
-
-    def authenticate_header(self, request):
-        return 'Bearer'
-
-    @classmethod
-    def create_jwt(cls, user):
-        payload = {
-            'user_identifier': user.username,
-            'exp': int((datetime.now() + timedelta(hours=settings.SIMPLE_JWT['TOKEN_LIFETIME_HOURS'])).timestamp()),
-            'iat': datetime.now().timestamp(),
-            'username': user.username,
-        }
-
-        jwt_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-
-        return jwt_token
-
-    @classmethod
-    def get_the_token_from_header(cls, token):
-        token = token.replace('Bearer', '').replace(' ', '')
-        return token
+def get_tokens_for_user(user):
+    access_token = AccessToken.for_user(user)
+    return {
+        'access': str(access_token)
+    }
