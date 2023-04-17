94b11417b5227ccd82e6704522e85c625f00b82b
Mikhail <mikhail@sudarkin.com>
2023-04-17 11:04:47 +0500

    Rebuild User models.

--- api_yamdb/review/models.py
+++ api_yamdb/review/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from api.permissions import IsUser, IsModerator, IsAdmin
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True)
+    bio = models.TextField(null=True, blank=True)
+    ROLE_CHOICES = [
+        (IsUser, 'user'),
+        (IsModerator, 'moderator'),
+        ('admin', IsAdmin)
+    ]
+    role = models.CharField(max_length=150, choices=ROLE_CHOICES, null=True, blank=True)
+    password = None
