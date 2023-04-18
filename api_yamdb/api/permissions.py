ec63343b85db5bcd40f5f9ab70195ace39ab82a9
Mikhail <mikhail@sudarkin.com>
2023-04-18 21:32:33 +0500

    Fix.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,12 +2,20 @@ from rest_framework import permissions
 
 
 class IsUser(permissions.IsAuthenticated):
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    or request.method in permissions.SAFE_METHODS)
+
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user == obj.author
+                    or request.method in permissions.SAFE_METHODS)
+
+class IsModerator(IsUser):
     pass
 
 
-class IsModerator():
-    pass
-
-
-class IsAdmin(permissions.IsAdminUser):
-    pass
+class IsAdmin(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.role == 'admin')
