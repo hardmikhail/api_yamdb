8691328b091d8d817a71c4730b6074fd2c090403
Mikhail <mikhail@sudarkin.com>
2023-04-27 22:20:42 +0500

    Added permissions.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,34 +1,45 @@
 from rest_framework import permissions
 
 
-class IsUser(permissions.IsAuthenticated):
-
+class ReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated
-                    or request.method in permissions.SAFE_METHODS)
+        return bool(request.method in permissions.SAFE_METHODS)
+
+    def has_object_permission(self, request, view, obj):
+        return bool(request.method in permissions.SAFE_METHODS)
 
 
-class IsModerator(IsUser):
-    pass
-
-
-class IsAdmin(permissions.BasePermission):
-
+class IsUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user
-                    and request.user.is_authenticated
-                    and request.user.is_admin
-                    or request.user.is_superuser)
+        return bool(request.user and request.user.is_authenticated)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated and (
-                    request.user.is_admin or request.user.is_superuser)))
+        return bool(request.method in permissions.SAFE_METHODS
+                    or request.user.is_authenticated
+                    and request.user.is_admin)
 
-class IsAuthor(permissions.IsAuthenticated):
+
+class IsAdminUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated
+                    and request.user.is_admin or request.user.is_staff)
 
     def has_object_permission(self, request, view, obj):
-        return bool(request.user and request.user.is_authenticated
-                    and request.user == obj.author)
+        return bool(request.user.is_admin
+                    or request.user.is_staff)
+
+
+class IsModerator(IsUser):
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user.is_moderator)
+
+
+class IsAdmin(IsUser, IsAdminUser):
+    pass
+
+
+class IsAuthor(IsUser):
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user == obj.author)
