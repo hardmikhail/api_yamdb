14567ac0b26bea2ef11faf6ff102bd33bfa67325
Сергей Сенягин <senyagin.serg@yandex.ru>
2023-04-28 04:09:02 +0300

    comments

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Categories, Genre, Title, User, Review, Comments, GenreTitle
+from .models import (User, Title, Categories, Genre, GenreTitle, Comments, Review)
 
-admin.site.register(GenreTitle)
-admin.site.register(Categories)
-admin.site.register(Comments)
-admin.site.register(Review)
+
 admin.site.register(User)
 admin.site.register(Title)
+admin.site.register(Categories)
 admin.site.register(Genre)
+admin.site.register(GenreTitle)
+admin.site.register(Review)
+admin.site.register(Comments)
